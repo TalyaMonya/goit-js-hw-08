e46cfc2462c276d9e6b2f4be5ff6066be70f22c6
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-08-15 13:59:56 +0300

    Done Task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle'
 
+const form = document.querySelector('.feedback-form');
+const KEY_LS = 'feedback-form-state';
+
+form.addEventListener('input', onTextInput);
+form.addEventListener('submit', throttle(onFormsubmit, 500));
+
+
+
+let data = JSON.parse(localStorage.getItem(KEY_LS)) ?? {};
+const { email, message } = form;
+
+email.value = data.email ?? '';
+message.value = data.message ?? '';
+
+
+
+function onTextInput(evt) {
+    data[evt.target.name] = evt.target.value;
+    localStorage.setItem(KEY_LS, JSON.stringify(data));
+}
+
+
+function onFormsubmit(evt) {
+    evt.preventDefault();
+    console.log(data);
+    form.reset();
+    localStorage.removeItem(KEY_LS);
+    data = {};
+}
